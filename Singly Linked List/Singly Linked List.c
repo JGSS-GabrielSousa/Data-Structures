ca324e2f2d9f9d13919cbb2e8313d7afcaae04c5
Gabriel Sousa <42483024+JGSS-GabrielSousa@users.noreply.github.com>
2020-04-17 22:54:11 -0300

    Add files via upload

---
+++ Singly Linked List/Singly Linked List.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    struct Node* next;
+    int value;
+}Node;
+
+typedef struct LinkedList{
+    int numberOfNodes;
+    struct Node* first;
+}LinkedList;
+
+void Append(LinkedList* list, int value){
+    Node* aux = (Node*)malloc(sizeof(Node));
+    aux->value = value;
+    aux->next = NULL;
+
+    if(list->numberOfNodes > 0){
+        Node* pointer = list->first;
+
+        while(pointer->next != NULL){
+            pointer = pointer->next;
+        }
+
+        pointer->next = aux;
+        list->numberOfNodes = list->numberOfNodes + 1;
+    }else{
+        list->first = aux;
+        list->numberOfNodes = list->numberOfNodes + 1;
+    }
+}
+
+void PrintList(LinkedList* list){
+    int i = 0;
+    Node* node;
+
+    printf("The list have %d nodes \n",list->numberOfNodes);
+
+    if(list->numberOfNodes > 0){
+        node = list->first;
+
+        printf("ID = Value \n",i++,node->value);
+        while(1){
+            printf("%d = %d \n",i++,node->value);
+
+            if(node->next != NULL)
+                node = node->next;
+            else
+                return;
+        }
+    }
+}
+
+int main(void){
+    printf("\t Singly Linked List \n\n");
+
+    LinkedList list = {.numberOfNodes = 0};
+
+    Append(&list,5);
+    Append(&list,10);
+    Append(&list,15);
+
+    PrintList(&list);
+
+    return 0;
+}
